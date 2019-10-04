41d96dff70da1d61561a36894bb07189f31c5591
victoryxh <55366027+victoryxh@users.noreply.github.com>
2019-10-04 18:21:38 -0400

    Update init.js

--- init.js
+++ init.js
@@ -1,25 +1,31 @@
 'use strict'
 
 const randomNumber = (upperBound) => {
-    return Math.floor(Math.random() * upperBound) + 1;
+  return Math.floor(Math.random() * upperBound) + 1;
 };
 
+// create an array storing first five numbers
 let lottery = [randomNumber(48), randomNumber(48), randomNumber(48), randomNumber(48), randomNumber(48)];
+
+// get the Lucky Ball number
 let lucky = randomNumber(18);
 document.write('Initial: <br>');
 
+// display the number array
 for (let number of lottery) {
-    document.write(`${number} `);
+  document.write(`${number} `);
 }
 
 document.write(lucky);
 document.write('<br><br>');
 
+// sort the number array
 lottery.sort((a, b) => a - b);
 
+// display the sorted number array
 document.write('Sorted: <br>')
 for (let number of lottery) {
-    document.write(`${number} `);
+  document.write(`${number} `);
 }
 
 document.write(lucky);
