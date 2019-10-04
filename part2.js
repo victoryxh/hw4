adc967d594f1a338d23918f236466fd39db74229
victoryxh <55366027+victoryxh@users.noreply.github.com>
2019-10-04 18:22:19 -0400

    Update part2.js

--- part2.js
+++ part2.js
@@ -1,22 +1,49 @@
-const calculateSubtotal = (hotdogCount, friesCount, drinkCount) => hotdogCount * 3.25 + friesCount * 2 +
-    drinkCount * 1.5;
+'use strict'
+
+// create an array storing menu items
+const itemsWithPrice = [{
+  name: 'Hotdog',
+  price: '3.25',
+}, {
+  name: 'French fries',
+  price: '2.00',
+}, {
+  name: 'Drinks',
+  price: '1.50',
+}];
+
+// loop over the array, printing out info about menu items
+for (let item of itemsWithPrice) {
+  let menuItem = document.createElement('p');
+  let node = document.createTextNode(`${item.name}: $${item.price}`);
+  menuItem.appendChild(node);
+  document.getElementById('menu').appendChild(menuItem);
+}
+
+const calculateSubtotal = (hotdog, fries, drinks) => hotdog * 3.25 + fries * 2 +
+  drinks * 1.5;
 
 const displayOrder = () => {
-    const hotdogCount = document.getElementById("hotDog").value;
-    const friesCount = document.getElementById("fries").value;
-    const drinkCount = document.getElementById("drink").value;
-    let subtotal = calculateSubtotal(hotdogCount, friesCount, drinkCount);
-    let total, discount, tax;
-    if (subtotal >= 20) {
-        total = subtotal * 0.9 * 1.0625;
-        discount = subtotal * 0.1;
-        tax = subtotal * 0.9 * 0.0625;
-    } else {
-        total = subtotal * 1.0625;
-        discount = 0;
-        tax = subtotal * 0.0625;
-    }
-    alert(`Items ordered: ${hotdogCount} hotdog, ${friesCount} French fries, ${drinkCount} drink.\n
+  // create an object storing quantities of menu items
+  const Quantity = {
+    hotdog: Number(document.getElementById('hotdog').value),
+    frenchFries: Number(document.getElementById('fries').value),
+    drinks: Number(document.getElementById('drink').value),
+  };
+  let subtotal = calculateSubtotal(Quantity.hotdog, Quantity.frenchFries, Quantity.drinks);
+  let total;
+  let discount;
+  let tax;
+  if (subtotal >= 20) {
+    total = subtotal * 0.9 * 1.0625;
+    discount = subtotal * 0.1;
+    tax = subtotal * 0.9 * 0.0625;
+  } else {
+    total = subtotal * 1.0625;
+    discount = 0;
+    tax = subtotal * 0.0625;
+  }
+  alert(`Items ordered: ${Quantity.hotdog} hotdog, ${Quantity.frenchFries} French fries, ${Quantity.drinks} drink.\n
                    subtotal: $${subtotal.toFixed(2)}\n
                    -\n
                    discount: $${discount.toFixed(2)}\n
